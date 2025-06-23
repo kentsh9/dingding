616d01918d41f587999e5a12f018731f58141c05
kentsh9 <jdshen2025@gmail.com>
2025-06-23 18:47:31 -0400

    Update dingCrypto.js

--- dingCrypto.js
+++ dingCrypto.js
@@ -1,50 +1,33 @@
 const crypto = require('crypto');
 
 class DingCrypto {
-  constructor(token, encodingAESKey, corpId) {
+  constructor(token, aesKey) {
     this.token = token;
-    this.key = Buffer.from(encodingAESKey + '=', 'base64');
-    this.iv = this.key.slice(0, 16);
-    this.corpId = corpId;
+    this.aesKey = Buffer.from(aesKey + '=', 'base64');
+    this.iv = this.aesKey.slice(0, 16);
   }
 
-  getSignature(timestamp, nonce, encrypt) {
-    const arr = [this.token, timestamp, nonce, encrypt].sort();
-    const str = arr.join('');
-    return crypto.createHash('sha1').update(str).digest('hex');
-  }
-
-  decrypt(text) {
-    const decipher = crypto.createDecipheriv('aes-256-cbc', this.key, this.iv);
+  decrypt(encrypt) {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', this.aesKey, this.iv);
     decipher.setAutoPadding(false);
-    let deciphered = Buffer.concat([decipher.update(text, 'base64'), decipher.final()]);
-
-    const pad = deciphered[deciphered.length - 1];
-    deciphered = deciphered.slice(0, deciphered.length - pad);
-
-    const content = deciphered.slice(16);
+    let decrypted = Buffer.concat([
+      decipher.update(encrypt, 'base64'),
+      decipher.final(),
+    ]);
+    const pad = decrypted[decrypted.length - 1];
+    decrypted = decrypted.slice(0, decrypted.length - pad);
+    const content = decrypted.slice(16);
     const length = content.slice(0, 4).readUInt32BE(0);
-    const result = content.slice(4, 4 + length);
-    return result.toString();
+    const message = content.slice(4, 4 + length).toString();
+    return message;
   }
 
-  encrypt(text) {
-    const random = crypto.randomBytes(16);
-    const msg = Buffer.from(text);
-    const msgLength = Buffer.alloc(4);
-    msgLength.writeUInt32BE(msg.length, 0);
-    const corpId = Buffer.from(this.corpId);
-    const rawMsg = Buffer.concat([random, msgLength, msg, corpId]);
-
-    const pad = 32 - (rawMsg.length % 32);
-    const padding = Buffer.alloc(pad, pad);
-    const finalMsg = Buffer.concat([rawMsg, padding]);
-
-    const cipher = crypto.createCipheriv('aes-256-cbc', this.key, this.iv);
-    cipher.setAutoPadding(false);
-    const encrypted = Buffer.concat([cipher.update(finalMsg), cipher.final()]);
-    return encrypted.toString('base64');
+  getToken() {
+    return this.token;
   }
 }
 
-module.exports = DingCrypto;
+module.exports = new DingCrypto(
+  'DtFVP3BXb3SSI4OIY6Oou6hJabHJXVCvJa5U2YB6i7P7ZjcZ', // token
+  'k7MLnoc5w2uAPGAbiLzb6GzmeoKzwudvm1or2z81jRA'       // aes_key
+);
