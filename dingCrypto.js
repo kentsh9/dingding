2d6ca17e7748b55aea1ac17d487a028dbbd274e5
kentsh9 <jdshen2025@gmail.com>
2025-06-23 20:17:32 -0400

    Update dingCrypto.js

--- dingCrypto.js
+++ dingCrypto.js
@@ -1,24 +1,26 @@
 const crypto = require('crypto');
 
 class DingCrypto {
-  constructor(token, aesKey, suiteKey = '') {
+  constructor(token, aesKey) {
     this.token = token;
-    this.suiteKey = suiteKey;
-    this.encodingAESKey = aesKey;
-    const AESKey_buffer = Buffer.from(aesKey + '=', 'base64');
-    this.key = AESKey_buffer;
-    this.iv = AESKey_buffer.slice(0, 16);
+    this.aesKey = Buffer.from(aesKey + '=', 'base64');
+    this.iv = this.aesKey.slice(0, 16);
   }
 
-  decrypt(text) {
-    const encrypted = Buffer.from(text, 'base64');
-    const decipher = crypto.createDecipheriv('aes-256-cbc', this.key, this.iv);
+  decrypt(encrypt) {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', this.aesKey, this.iv);
     decipher.setAutoPadding(false);
-    let decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
+    let decrypted = Buffer.concat([decipher.update(encrypt, 'base64'), decipher.final()]);
 
+    // 去除 PKCS#7 Padding
     const pad = decrypted[decrypted.length - 1];
-    decrypted = decrypted.slice(20, decrypted.length - pad);
-    return JSON.parse(decrypted.toString());
+    decrypted = decrypted.slice(0, decrypted.length - pad);
+
+    const content = decrypted.slice(16);
+    const length = content.slice(0, 4).readUInt32BE(0);
+    const message = content.slice(4, 4 + length).toString();
+
+    return message;
   }
 }
 
